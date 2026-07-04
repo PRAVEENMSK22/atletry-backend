10f3b391c4cf7f8fa00f2cd9b68ebd05ed5fda2a
Ubuntu <ubuntu@ip-172-31-12-105.ec2.internal>
2026-07-04 09:47:33 +0000

    Added test endpoint for CI/CD demo

---
+++ src/main/java/com/atletry/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.atletry.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Deployment Success - Version 2";
+    }
+}
